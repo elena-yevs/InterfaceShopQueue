6ca91f1678629a678588530e30a9e05cb87a930e
Elena Yevsyukova <elena.yevs@gmail.com>
2020-11-24 11:31:10 +0200

    start setting the interface for implementing the shop queue.

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer customer=new Customer();
+        customer.head();
+        customer.tail();
+        customer.serve();
+    }
+}
---
+++ src/com/company/ShopQueue.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface ShopQueue {
+    void head();
+    void tail();
+    void serve();
+
+}
