5a3085fd9abacc659309a75ae4b28b9e1d3707b8
Elena Yevsyukova <elena.yevs@gmail.com>
2020-11-26 16:09:33 +0200

    Customer is a class that takes LoyaltyCard as a constructor parameter. The interface has no implementation.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,9 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer=new Customer();
-        customer.head();
-        customer.tail();
-        customer.serve();
+
+        Customer customer=new Customer("545");
+
     }
 }
